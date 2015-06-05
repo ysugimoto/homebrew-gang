0e74e44d235c100a2194b4e41fa05d0ac86781ac
sugimoto <sugimoto@wnotes.net>
2015-06-06 01:44:08 +0900

    Bump version

--- gang.rb
+++ gang.rb
@@ -3,11 +3,11 @@ require "formula"
 class Gang < Formula
   homepage ""
   if Hardware.is_64_bit?
-    url "https://github.com/ysugimoto/gang/releases/download/v0.8/gang_amd64.tar.gz"
-    sha256 "9e9496fbf2fcacbbfad722c67a1b3dc7a097bf9804f819de69dcd53718a9201d"
+    url "https://github.com/ysugimoto/gang/releases/download/v0.8.1/gang_amd64.tar.gz"
+    sha256 "3196924090f87ebe9196460b3146918eb63e1e577365d7d1078008437e81deed"
   else
-    url "https://github.com/ysugimoto/gang/releases/download/v0.8/gang_i386.tar.gz"
-    sha256 "cb7c78df10a256d4dab38b7f2ac696701cc2659c88250268a96679ac0aa9c86d"
+    url "https://github.com/ysugimoto/gang/releases/download/v0.8.1/gang_i386.tar.gz"
+    sha256 "afdb097293df912c7b13eda0c098870286ac74132d52ab675f863ad985f810e7"
   end
 
   def install
